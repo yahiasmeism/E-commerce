d69cfad5da625641a4f4fcd68aca380c7ed05bd1
yahiasmeism <yahiasmeism@gmail.com>
2024-06-25 22:34:36 +0300

    register External and Core objects to service_locator

--- lib/core/services/service_locator.dart
+++ lib/core/services/service_locator.dart
@@ -1,4 +1,8 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../network/network_info.dart';
 
 final sl = GetIt.instance;
 
@@ -55,6 +59,12 @@ Future<void> init() async {
 
   ///****************************************************
   ///! Core
+  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl());
 
   ///! External
+  sl.registerLazySingleton<Dio>(() => Dio());
+  final sharedPreferences = await SharedPreferences.getInstance();
+  sl.registerLazySingleton<SharedPreferences>(
+    () => sharedPreferences,
+  );
 }
